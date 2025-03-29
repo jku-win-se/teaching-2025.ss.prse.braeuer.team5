e6779bf08ee41afc3ab90a7ff4f8acd1bc9af64c
MagSal <magdalena.salomon@outlook.at>
2025-03-29 20:36:03 +0100

    UserStory 18 User.java, Administrator..java Klasse angelegt - Methoden, die diese UserStory nicht betreffen sind noch offen Prototype erweitert mit DeleteAdminUser.fxml, DeleteAdminUserController, AddAdminUser.fxml, AdminDashboardController.java dashboard2.fxml angepasst - Button für User löschen und hinzufügen haben noch gefehlt UserRepository.java erstellt um die Userdaten von der DB abzufragen start.fxml hatte Fehlermeldungen - also habe ich die Buttonbennung entfernt

---
+++ src/main/java/jku/se/Controller/AddAdminUserController.java
@@ -0,0 +1,69 @@
+package jku.se.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import jku.se.User;
+import jku.se.repository.UserRepository;
+import javafx.scene.control.Label;
+import java.io.IOException;
+
+public class AddAdminUserController { //#18 Magdalena
+
+    @FXML private TextField txtEmail;
+    @FXML private TextField txtName;
+    @FXML private TextField txtPassword;
+    @FXML private CheckBox chkAdmin;
+    @FXML private CheckBox chkUser;
+    @FXML private Label message;
+
+    //add user
+    @FXML
+    private void addAdminUser(ActionEvent event) throws IOException{
+        String email = txtEmail.getText();
+        String name = txtName.getText();
+        String password = txtPassword.getText();
+        boolean isAdmin = chkAdmin.isSelected();
+
+
+        if (email.isEmpty() || name.isEmpty() || password.isEmpty()) {
+            message.setText("Please fill in all fields!");
+            return;
+        }
+
+        User newUser = new User(name, email, password, isAdmin);
+        UserRepository.addUser(newUser);  // add User in database
+        message.setText("User added successfully!");
+        clearFields();
+    }
+
+    //delete the fields after insert
+    private void clearFields() {
+        txtEmail.clear();
+        txtName.clear();
+        txtPassword.clear();
+        chkAdmin.setSelected(false);
+        chkUser.setSelected(false);
+    }
+    @FXML //exit
+    private void cancelAddAdminUser(ActionEvent event) throws IOException {
+        loadPage("dashboard2.fxml", event);
+    }
+
+    @FXML
+    private void loadPage(String fxmlFile, ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/" + fxmlFile));
+        Scene scene = new Scene(loader.load());
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
