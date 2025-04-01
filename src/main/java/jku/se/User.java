5f12d44d68ddc7e3e9238f80e5a3436266cc2d16
fridakem <frida.kempinger@icloud.com>
2025-04-01 15:59:37 +0200

    Issue 8 & 22

--- src/main/java/jku/se/User.java
+++ src/main/java/jku/se/User.java
@@ -10,12 +10,15 @@ public class User { //Magdalena #18
     private boolean isAdministrator;
     private List<Invoice> invoices;
 
+    private String preferredNotificationMethod;
+
     public User(String name, String email, String password, boolean isAdministrator) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.isAdministrator = isAdministrator;
         this.invoices = new ArrayList<>();
+        this.preferredNotificationMethod = "In-App";
     }
 
     // Getter & Setter
@@ -23,13 +26,19 @@ public class User { //Magdalena #18
     public String getEmail() { return email; }
     public String getPassword(){return password;}
     public boolean isAdministrator() { return isAdministrator; }
+    public String getPreferredNotificationMethod(){return preferredNotificationMethod;}
 
     public void login() { //frida #3
         System.out.println(name + " hat sich eingeloggt.");
     }
 
     public void uploadInvoice(Invoice invoice) { //jovana #4
+        invoices.add(invoice);
 
+        double amount = invoice.calculateRefund();
+        String message = "Deine Rechnung über " + amount + " € wurde erfolgreich eingereicht.";
+
+        receiveNotification(message);
     }
 
     public List<Invoice> viewHistory() { //jovana #11??
@@ -37,11 +46,30 @@ public class User { //Magdalena #18
     }
 
     public void receiveNotification(String message) { // jovana # 8
-
+        Notification notification = new Notification();
+        switch (preferredNotificationMethod){
+            case "In-App":
+                notification.sendInApp(this, message);
+                break;
+            case "Email":
+                notification.sendEmail(this, message);
+                break;
+            case "Both":
+                notification.sendInApp(this, message);
+                notification.sendEmail(this, message);
+                break;
+            default:
+                notification.sendInApp(this,message);
+        }
     }
 
-    public void changeNotificationSettings() { //jovana - #22
-
+    public void changeNotificationSettings(String newPreference) { //jovana - #22
+        if(newPreference.equals("Email") || newPreference.equals("In-App")|| newPreference.equals("Both")){
+            this.preferredNotificationMethod = newPreference;
+            System.out.println("Benachrichtigungspräferenz geändert zu: " + newPreference);
+        }else{
+            System.out.println("Ungültige Eingabe. Mögliche Optionen: Email, In-App, Both");
+        }
     }
 
 }
