101cb3c1af381c07b5761154a2fd389a28530642
MagSal <magdalena.salomon@outlook.at>
2025-04-01 16:13:27 +0200

    UserStory 15

--- src/main/java/jku/se/repository/InvoiceRepository.java
+++ src/main/java/jku/se/repository/InvoiceRepository.java
@@ -1,11 +1,45 @@
 package jku.se.repository;
 
+import jku.se.DatabaseConnection;
 import jku.se.Invoice;
 
-public class InvoiceRepository {
-    public void saveInvoice(Invoice invoice) {
-        // Hier die Logik zum Speichern der Rechnung in der Datenbank
-        // z.B. mit JDBC, Hibernate, etc.
-        System.out.println("Rechnung gespeichert: " + invoice);
-    }
-}
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InvoiceRepository { //#15 - Magdalena
+
+    private static final String SELECT_ALL_INVOICES = "SELECT *  FROM invoice";
+
+    public static List<Invoice> getAllInvoices() {
+        List<Invoice> invoices = new ArrayList<>();
+
+        try (Connection con = DatabaseConnection.getConnection();
+             PreparedStatement stmt = con.prepareStatement(SELECT_ALL_INVOICES);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+
+                Invoice invoice = new Invoice(rs.getString("user_email"),
+                        rs.getDate("date").toLocalDate(),
+                        rs.getDouble("amount"),
+                        rs.getString("category"),
+                        rs.getString("status"),
+                        rs.getString("fileUrl"),
+                        rs.getDate("createdAt").toLocalDate().atStartOfDay(),
+                        rs.getDouble("reimbursement")
+                );
+                invoices.add(invoice);
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error retrieving invoices: " + e.getMessage());
+        }
+
+        return invoices;
+    }}
