1fb1a92c3789b5caba37dedc39ae90cb98a18a41
fridakem <frida.kempinger@icloud.com>
2025-04-07 14:40:42 +0200

    Methoden hinzugefügt in AnomalyDetection

--- src/main/java/jku/se/AnomalyDetection.java
+++ src/main/java/jku/se/AnomalyDetection.java
@@ -1,4 +1,11 @@
 package jku.se;
 
 public class AnomalyDetection {
+    boolean detectDuplicateUpload(User user, Invoice invoice){
+        return false;
+    }
+
+    boolean detectMismatch(Invoice invoice){
+        return false;
+    }
 }
