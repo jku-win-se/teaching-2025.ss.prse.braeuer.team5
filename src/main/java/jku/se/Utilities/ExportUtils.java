3849e9fe02ee78b40232e5d143a7349c5ea72540
mihjov13 <mihajlovicjov13@gmail.com>
2025-05-16 21:05:25 +0200

    Export Invoice Data Admin

--- src/main/java/jku/se/Utilities/ExportUtils.java
+++ src/main/java/jku/se/Utilities/ExportUtils.java
@@ -11,42 +11,51 @@ import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ExportUtils {
     private static final Logger logger = Logger.getLogger(ExportUtils.class.getName());
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd_HHmmss");
+    private static final int PDF_FONT_SIZE = 12;
+    private static final int PDF_TITLE_SIZE = 16;
+    private static final int PDF_MARGIN = 50;
+    private static final int PDF_LINE_SPACING = 20;
 
+    /**
+     * Export data to JSON file
+     */
     public static boolean exportToJson(Map<String, ?> data, String baseFileName) throws IOException {
         String fileName = generateFileName(baseFileName, "json");
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(data);
-
-        try (FileWriter writer = new FileWriter(fileName)) {
-            writer.write(json);
-            return true;
-        }
+        return writeToFile(fileName, json);
     }
 
+    /**
+     * Export data to CSV file
+     */
     public static boolean exportToCsv(Map<String, ?> data, String baseFileName) throws IOException {
         String fileName = generateFileName(baseFileName, "csv");
         StringBuilder csv = new StringBuilder("Key,Value\n");
 
         for (Map.Entry<String, ?> entry : data.entrySet()) {
-            csv.append(escapeCsv(entry.getKey())).append(",")
-                    .append(escapeCsv(entry.getValue().toString())).append("\n");
+            csv.append(escapeCsv(entry.getKey()))
+                    .append(",")
+                    .append(escapeCsv(String.valueOf(entry.getValue())))
+                    .append("\n");
         }
-
-        try (FileWriter writer = new FileWriter(fileName)) {
-            writer.write(csv.toString());
-            return true;
-        }
+        return writeToFile(fileName, csv.toString());
     }
 
+    /**
+     * Export data to PDF file
+     */
     public static boolean exportToPdf(Map<String, ?> data, String title, String baseFileName) throws IOException {
         String fileName = generateFileName(baseFileName, "pdf");
 
@@ -57,40 +66,72 @@ public class ExportUtils {
             PDPageContentStream contentStream = new PDPageContentStream(document, page);
             try {
                 // Header
-                contentStream.setFont(PDType1Font.HELVETICA_BOLD, 16);
+                contentStream.setFont(PDType1Font.HELVETICA_BOLD, PDF_TITLE_SIZE);
                 contentStream.beginText();
-                contentStream.newLineAtOffset(50, 750);
+                contentStream.newLineAtOffset(PDF_MARGIN, 800);
                 contentStream.showText(title);
                 contentStream.endText();
 
                 // Content
-                contentStream.setFont(PDType1Font.HELVETICA, 12);
+                contentStream.setFont(PDType1Font.HELVETICA, PDF_FONT_SIZE);
                 contentStream.beginText();
-                contentStream.newLineAtOffset(50, 700);
+                contentStream.newLineAtOffset(PDF_MARGIN, 750);
 
-                int yPosition = 700;
+                int yPosition = 750;
                 for (Map.Entry<String, ?> entry : data.entrySet()) {
-                    contentStream.showText(entry.getKey() + ": " + entry.getValue());
-                    yPosition -= 20;
-                    contentStream.newLineAtOffset(0, -20);
+                    if (yPosition < PDF_MARGIN) {
+                        contentStream.endText();
+                        contentStream.close();
+
+                        // Neue Seite erstellen
+                        PDPage newPage = new PDPage(PDRectangle.A4);
+                        document.addPage(newPage);
+                        contentStream = new PDPageContentStream(document, newPage);
+
+                        // Neue Seite initialisieren
+                        contentStream.setFont(PDType1Font.HELVETICA, PDF_FONT_SIZE);
+                        contentStream.beginText();
+                        yPosition = 750;
+                        contentStream.newLineAtOffset(PDF_MARGIN, yPosition);
+                    }
+
+                    String line = String.format("%-30s: %s", entry.getKey(), entry.getValue());
+                    contentStream.showText(line);
+                    yPosition -= PDF_LINE_SPACING;
+                    contentStream.newLineAtOffset(0, -PDF_LINE_SPACING);
                 }
                 contentStream.endText();
             } finally {
-                contentStream.close();
+                if (contentStream != null) {
+                    contentStream.close();
+                }
             }
 
             document.save(new File(fileName));
             return true;
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "PDF export failed", e);
+            throw e;
         }
     }
 
+    // Helper methods
     private static String generateFileName(String baseName, String extension) {
         String timestamp = DATE_FORMAT.format(new Date());
-        return Paths.get(System.getProperty("user.home"), "Downloads",
-                String.format("%s_%s.%s", baseName, timestamp, extension)).toString();
+        Path path = Paths.get(System.getProperty("user.home"), "Downloads",
+                String.format("%s_%s.%s", baseName, timestamp, extension));
+        return path.toString();
+    }
+
+    private static boolean writeToFile(String fileName, String content) throws IOException {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(content);
+            return true;
+        }
     }
 
     private static String escapeCsv(String value) {
+        if (value == null) return "";
         if (value.contains(",") || value.contains("\"") || value.contains("\n")) {
             return "\"" + value.replace("\"", "\"\"") + "\"";
         }
